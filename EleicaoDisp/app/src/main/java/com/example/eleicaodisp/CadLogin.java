145448392f5698af36230a0d2ee3782c2d705e3d
EricCs04 <ericcsantos2504@gmail.com>
2024-10-04 10:51:52 -0300

    2nd version

--- EleicaoDisp/app/src/main/java/com/example/eleicaodisp/CadLogin.java
+++ EleicaoDisp/app/src/main/java/com/example/eleicaodisp/CadLogin.java
@@ -1,8 +1,20 @@
-package com.example.eleicao;
+package com.example.eleicaodisp;
 
+
+
+import android.provider.Settings;
 
 public class CadLogin {
 
+    public static String usuario[] = {"admin", "entrevistado"};
+    public static String senha[] = {"admin" , "entrevistado"};
 
 
+    public String[] getUsuario() {
+        return usuario;
+    }
+
+    public String[] getSenha() {
+        return senha;
+    }
 }
--- EleicaoDisp/app/src/main/java/com/example/eleicaodisp/Login.java
+++ EleicaoDisp/app/src/main/java/com/example/eleicaodisp/Login.java
@@ -1,5 +1,8 @@
 package com.example.eleicaodisp;
 
+
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.stream.IntStream;
+
 public class Login extends AppCompatActivity {
 
      Button btConectar;
      EditText etUsuario, etSenha;
+     CadLogin usuario[] , senha[];
 
 
     @Override
@@ -27,11 +33,33 @@ public class Login extends AppCompatActivity {
         etUsuario = (EditText)findViewById(R.id.etUsuario);
         etSenha = (EditText)findViewById(R.id.etSenha);
 
-
         btConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                boolean loginBS= false;
 
+                for (int i = 0; i < CadLogin.usuario.length; i++) {
+                    if (etUsuario.getText().toString().equals(CadLogin.usuario[i]) && etSenha.getText().toString().equals(CadLogin.senha[i])) {
+                        loginBS = true;
+                        if (etUsuario.getText().toString().equals("entrevistado")) {
+                            Intent proxPage = new Intent(Login.this, InfoEntrevistado.class);
+                            startActivity(proxPage);
+                            break;
+                        }else if (etUsuario.getText().toString().equals("admin")) {
+                            Intent res = new Intent(Login.this, Resultado.class);
+                            startActivity(res);
+                            break;
+                        }
+                    }
+                }
+                if(!loginBS){
+                    AlertDialog alertDialog = new AlertDialog.Builder(Login.this).create();
+                    alertDialog.setTitle("ERRO");
+                    alertDialog.setMessage("O Usuário ou Senha está Errado.");
+                    alertDialog.show();
+                    etSenha.setText("");
+                    etUsuario.setText("");
+                }
             }
         });
 
